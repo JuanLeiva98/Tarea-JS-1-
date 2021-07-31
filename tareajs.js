c499754ae4765af3ff717a40930e60e771d7d30d
Juan Leiva <jleiva@marfrig.com>
2021-07-30 23:56:12 -0300

    cambios

---
+++ tareajs.js
@@ -0,0 +1,35 @@
+/*var distancia
+
+distancia = prompt ('Ingrese la distancia a recorrer en metros');
+
+if (distancia <= 1000){
+    document.write ('Usted debe ir a pie');
+}
+    else
+    {
+        if (distancia <= 10000){
+        document.write ('Usted debe ir en bicicleta');
+    
+    }   
+        else  {
+            if (distancia <= 30000 ){
+                document.write ('Usted debe ir en autobús');
+        }
+            else {
+                if (distancia <= 100000){
+                document.write ('Usted debe ir en automovil')
+            }
+            else {
+                document.write ('Usted debe ir en avíon')
+            }
+        }
+
+        }
+    }
+*/
+
+var numeros = [30, 15, 96, 85, 14];
+
+for (var i = 0; i < 5; i++){
+console.log ('El numero mayor es' + ' ' + numeros[2])
+}
